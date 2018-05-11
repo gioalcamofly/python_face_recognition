78c9c83c7dae1d1c272f5a0bed10434e098ee3f7
gioalcamofly <giovanni.alberto@hotmail.com>
2018-05-11 20:57:01 +0100

    new version

--- video_software.py
+++ video_software.py
@@ -44,19 +44,15 @@ def find():
     face_encodings = []
     face_names = []
     process_this_frame = True
-    video_capture = cv2.VideoCapture(1)
+    video_capture = cv2.VideoCapture(0)
     tries = 0
     while True:
-        # Grab a single frame of video
         ret, frame = video_capture.read()
 
-        # Resize frame of video to 1/4 size for faster face recognition processing
         small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
-        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
         rgb_small_frame = small_frame[:, :, ::-1]
 
-        # Only process every other frame of video to save time
         if process_this_frame:
             # Find all the faces and face encodings in the current frame of video
             face_locations = face_recognition.face_locations(rgb_small_frame)
@@ -67,7 +63,7 @@ def find():
             face_names = []
             for face_encoding in face_encodings:
                 # See if the face is a match for the known face(s)
-                matches = face_recognition.compare_faces(face_encodings_array, face_encoding)
+                matches = face_recognition.compare_faces(face_encodings_array, face_encoding, tolerance=0.5)
                 name = "Unknown"
 
                 # If a match was found in known_face_encodings, just use the first one.
